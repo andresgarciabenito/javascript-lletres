e560ab59fa23b451f28929e6822bd76db89ae504
andresgarciabenito <andresgarciabenito@hotmail.com>
2020-06-05 13:47:31 +0200

    fase2b

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
 //FASE 1
 //a)Crear un array con tu nombre donde cada posición responda a una letra
-const name=["A", "N", "D", "R", "E", "S"];
+const name=["A", "N", "D", "R", "E", "S", 3];
 //b)haz un bucle que recorra este array y muestre por consola cada una de las letras.
 
 // for (var i=0; i<name.length; i++){
@@ -12,10 +12,24 @@ const name=["A", "N", "D", "R", "E", "S"];
 
 //FASE 2
 //a)Al bucle, si la letra es vocal, imprime en la consola: "He encontrado la vocal:__", sino "He encontrado la consonante:__";
+// for (var i=0; i<name.length; i++){
+//    var char=name[i];
+//    if(char === "A"||char === "E"||char === "I"||char === "O"||char === "U"){
+//     console.log("He encontrado la VOCAL: "+name[i]+".");
+//   }else
+//    console.log("He encontrado la CONSONANTE: "+name[i]+".");
+// }
+
+
+//b)Si encuentras un número, muestra por pantala "Los nombres de personas no contienen el número:__"
+
 for (var i=0; i<name.length; i++){
-   var char=name[i];
-   if(char === "A"||char === "E"||char === "I"||char === "O"||char === "U"){
-    console.log("He encontrado la VOCAL: "+name[i]+".");
-  }else
-   console.log("He encontrado la CONSONANTE: "+name[i]+".");
+    var char=parseInt(name[i]);
+
+      if (!Number.isNaN(char)) {
+          console.log("Los nombres de personas no contienen el número: "+name[i]+".");
+      }else if(name[i] === "A"||name[i] === "E"||name[i] === "I"||name[i] === "O"||name[i] === "U"){
+          console.log("He encontrado la VOCAL: "+name[i]+".");
+      }else
+        console.log("He encontrado la CONSONANTE: "+name[i]+".");
 }
