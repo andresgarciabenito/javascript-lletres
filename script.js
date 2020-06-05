e36e38c7efcd258c9c31117a3d193d46484fbbbc
andresgarciabenito <andresgarciabenito@hotmail.com>
2020-06-05 13:44:22 +0200

    fase1

---
+++ script.js
@@ -0,0 +1,12 @@
+//Ejercicio letras repetidas
+//Monstrar por consola cuantas veces aparece cada letra de tu nombre, con distintas variables (fases)
+
+//FASE 1
+//a)Crear un array con tu nombre donde cada posición responda a una letra
+const name=["A", "N", "D", "R", "E", "S"];
+
+//b)haz un bucle que recorra este array y muestre por consola cada una de las letras.
+
+for (var i=0; i<name.length; i++){
+  console.log(name[i]);
+}
